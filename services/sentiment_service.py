219f4e906bd04d4fddca508fdb1bb6ffe1d9dc1b
Navjot Kaur <141299269+navjot6@users.noreply.github.com>
2026-05-28 14:46:50 +0530

    Add files via upload

---
+++ services/sentiment_service.py
@@ -0,0 +1,12 @@
+from textblob import TextBlob
+def analyze_sentiment(text):
+
+    score = TextBlob(text).sentiment.polarity
+
+    if score > 0:
+        return "Positive"
+
+    elif score < 0:
+        return "Negative"
+
+    return "Neutral"
